85536b643abce5e33d7b7c864416d5f822d91d02
CycleBird <jzzconstantine@gmail.com>
2024-04-05 01:15:30 -0600

    Trans SQLite to MySQL

--- backend/backend/settings.py
+++ backend/backend/settings.py
@@ -11,6 +11,8 @@ https://docs.djangoproject.com/en/4.2/ref/settings/
 """
 
 from pathlib import Path
+import json
+import os
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
@@ -82,10 +84,21 @@ WSGI_APPLICATION = 'backend.wsgi.application'
 # Database
 # https://docs.djangoproject.com/en/4.2/ref/settings/#databases
 
+config_path = BASE_DIR / 'config.json'
+print(f"Attempting to open config file at: {config_path}") #check path
+with open(config_path) as config_file:
+    config = json.load(config_file)
+
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
+        # 'ENGINE': 'django.db.backends.sqlite3',
+        # 'NAME': BASE_DIR / 'db.sqlite3',
+        'ENGINE': 'django.db.backends.mysql',
+        'NAME': config['DEFAULT_SCHEMA'],
+        'USER': config['USERNAME'],
+        'PASSWORD': config['PASSWORD'],
+        'HOST': config['HOSTNAME'],
+        'PORT': config['PORT'],
     }
 }
 
--- backend/flashcards/tests.py
+++ backend/flashcards/tests.py
@@ -1,3 +1,39 @@
+from django.contrib.auth.models import User
 from django.test import TestCase
+from flashcards.models import Deck, Card, Folder, SharedDeck
 
 # Create your tests here.
+
+class FlashcardsTestCase(TestCase):
+    # Set up data for the whole TestCase
+    def setUp(self):
+        self.owner = User.objects.create_user('jzz', 'jzz@test.com', 'jpassword')
+        self.folder = Folder.objects.create(name='Test Folder', owner=self.owner)
+        self.deck = Deck.objects.create(name='Test Deck', description='A test deck', owner=self.owner, folder=self.folder)
+        self.card = Card.objects.create(question='Who am I??', answer='Pikachu!', deck=self.deck, bucket=1)
+        self.shared_with_user = User.objects.create_user('seth', 'seth@test.com', 'spassword')
+        self.shared_deck = SharedDeck.objects.create(deck=self.deck, shared_with=self.shared_with_user)
+
+    # Test folder creation
+    def test_folder_creation(self):
+        self.assertTrue(Folder.objects.filter(name='Test Folder').exists())
+        self.assertEqual(Folder.objects.count(), 1)
+        self.assertEqual(Folder.objects.first(), self.folder)
+
+    # Test deck creation
+    def test_deck_creation(self):
+        self.assertTrue(Deck.objects.filter(name='Test Deck').exists())
+        self.assertEqual(Deck.objects.count(), 1)
+        self.assertEqual(Deck.objects.first(), self.deck)
+
+    # Test card creation
+    def test_card_creation(self):
+        self.assertTrue(Card.objects.filter(question='Who am I??').exists())
+        self.assertEqual(Card.objects.count(), 1)
+        self.assertEqual(Card.objects.first(), self.card)
+
+    # Test shared deck creation
+    def test_shared_deck_creation(self):
+        self.assertTrue(SharedDeck.objects.filter(deck=self.deck, shared_with=self.shared_with_user).exists())
+        self.assertEqual(SharedDeck.objects.count(), 1)
+        self.assertEqual(SharedDeck.objects.first(), self.shared_deck)
