6d7ea4167fdd862d461804c578ee1b54153c2003
NightlXC <w1252801556@gmail.com>
2024-04-19 11:40:05 -0600

    Maybe review page?

--- frontend/src/components/ReviewPage.jsx
+++ frontend/src/components/ReviewPage.jsx
@@ -1,20 +1,20 @@
 import { useState, useRef } from "react";
 import { useMutation, useQuery } from 'react-query';
 import { Link, useParams } from "react-router-dom";
-import "./ReviewPage.css";
+// import "./ReviewPage.css";
 import Sidebar from "./SideBar";
 
 function FinishView() {
   return (
-    <div className="finishView"> 
-      <img className="partyPopper" src="../party-popper-flip.png" alt="Party Popper" />
-      <div className="finishViewMiddle">
-        <h3 className="finishText">You have studied all the cards in this deck</h3>
+    <div className="flex flex-row justify-center items-center -mt-[10vh]"> 
+      <img className="w-1/3 h-1/2 mt-[-10vh]" src="../party-popper-flip.png" alt="Party Popper" />
+      <div className="flex flex-col justify-center items-center mx-4">
+        <h3 className="h-[25vh] flex justify-center items-center w-full border-black bg-white rounded-md p-5 text-2xl text-black my-4">You have studied all the cards in this deck</h3>
         <Link to="/home">
           <button className="border rounded-md px-2 py-1">Back to deck</button>
         </Link>
       </div>
-      <img className="partyPopper" src="../party-popper.png" alt="Party Popper" />
+      <img className="w-1/3 h-1/2 mt-[-10vh]" src="../party-popper.png" alt="Party Popper" />
     </div>
   )
 }
@@ -75,23 +75,25 @@ function ReviewCard({ card, showAnswer, setShowAnswer, updateReviewedCard }) {
 
   return (
     <div className="flex flex-col items-center">
-      <div className={`reviewCard ${showAnswer ? "show" : "hide"}`}>
-          <p className="question h-20">{card.question}</p>
-          <p className="answer h-20">{card.answer}</p>
+      <div className={`flex flex-col items-center h-auto w-80 mx-auto`}>
+        <div className="w-full">
+          <p className={`bg-white rounded-md px-4 py-2 flex justify-center items-center text-black h-20 mb-12 transition-margin duration-500 ease-in-out ${showAnswer ? "-mt-24" : "mt-0"}`}>{card.question}</p>
+          <p className={`bg-white rounded-md px-4 py-2 flex justify-center items-center text-black h-20 ${showAnswer ? "opacity-100 transition-opacity duration-500" : "opacity-0"}`}>{card.answer}</p>
+        </div>
       </div>
       {!showAnswer && <button className="mt-8 border rounded-md w-[50%]" onClick={changeShowAnswer}>Reveal Answer</button>}
       {showAnswer && (
-        <div className="flex justify-center mt-8">
-          <button className="againButton" onClick={() => updateReviewedCard(0, getNextReviewTime(1), card)}>Again <br /> 
+        <div className="flex justify-center mt-28">
+          <button className="px-4 mr-4 text-black bg-red-600 hover:bg-red-700" onClick={() => updateReviewedCard(0, getNextReviewTime(1), card)}>Again <br /> 
             {formatTimeDifference(now.getTime(), getNextReviewTime(1))}</button>
 
-          <button className="hardButton" onClick={() => updateReviewedCard(card.bucket + 1, getNextReviewTime(2), card)}>Hard <br /> 
+          <button className="px-4 mr-4 text-black bg-yellow-400 hover:bg-yellow-500" onClick={() => updateReviewedCard(card.bucket + 1, getNextReviewTime(2), card)}>Hard <br /> 
             {formatTimeDifference(now.getTime(), getNextReviewTime(2))}</button>
 
-          <button className="goodButton" onClick={() => updateReviewedCard(card.bucket + 1, getNextReviewTime(3), card)}>Good <br /> 
+          <button className="px-4 mr-4 text-black bg-green-700 hover:bg-green-800" onClick={() => updateReviewedCard(card.bucket + 1, getNextReviewTime(3), card)}>Good <br /> 
             {formatTimeDifference(now.getTime(), getNextReviewTime(3))}</button>
 
-          <button className="easyButton" onClick={() => updateReviewedCard(card.bucket + 1, getNextReviewTime(4), card)}>Easy <br /> 
+          <button className="px-4 text-black bg-green-400 hover:bg-green-500" onClick={() => updateReviewedCard(card.bucket + 1, getNextReviewTime(4), card)}>Easy <br /> 
             {formatTimeDifference(now.getTime(), getNextReviewTime(4))}</button>
         </div>
       )}
@@ -159,7 +161,7 @@ function Review() {
     <div>
       <Sidebar />
       <div className="border rounded-lg p-8 mt-[10vh] h-[60vh] w-[40vw] flex flex-col">
-        <h2 className="mb-40 text-center text-[2em]">{reviews.deck_name}</h2>
+        <h2 className="mb-[20vh] text-center text-[2em]">{reviews.deck_name}</h2>
         {!finish && (
           <ReviewCard
             card={reviews.cards[cardIndex]}
