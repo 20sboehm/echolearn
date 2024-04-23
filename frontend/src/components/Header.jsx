28b17bf0b59e4a931c2ef848a41d1c94c01527df
NightlXC <w1252801556@gmail.com>
2024-04-22 20:17:34 -0600

    Fix the sign up for the header

--- frontend/src/components/Header.jsx
+++ frontend/src/components/Header.jsx
@@ -21,7 +21,7 @@ function Header() {
         
           <div className="flex items-center space-x-4 justify-end mr-10">
             <Link to="/login" className="px-4 py-2 text-black font-bold transition duration-100 hover:bg-gray-300">Log in</Link>
-            <Link to="/home" className="px-4 py-2 bg-userHeaderColor text-white rounded-lg font-bold transition duration-100 hover:text-black py-2">Sign in</Link>
+            <Link to="/" className="px-4 py-2 bg-userHeaderColor text-white rounded-lg font-bold transition duration-100 hover:text-black py-2">Sign up</Link>
           </div>
         </div>
       </header>
