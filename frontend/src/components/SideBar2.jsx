3b582777ab4061fbc59a2ba397a1e28692692be7
NightlXC <w1252801556@gmail.com>
2024-09-05 10:25:03 -0600

    Able to adjust the width

--- frontend/src/components/SideBar2.jsx
+++ frontend/src/components/SideBar2.jsx
@@ -1,9 +1,12 @@
 import React, { useState, useEffect } from 'react';
 import { useQuery } from "react-query";
+import { Link } from "react-router-dom";
 import { useApi } from "../hooks";
 import folderOpenImg from "../assets/folder-open.png";
 import folderCloseImg from "../assets/folder-close.png";
 import decksImg from "../assets/decks.png";
+import { ResizableBox } from 'react-resizable';
+import 'react-resizable/css/styles.css';
 
 const Folder = ({ folder }) => {
   const [openFolder, setOpenFolder] = useState(false);
@@ -13,27 +16,29 @@ const Folder = ({ folder }) => {
   };
 
   return (
-    <div>
+    <div className="mt-2">
       <div onClick={handleOpenFolder} className="cursor-pointer text-black flex">
-      <img
+        <img
           src={openFolder ? folderOpenImg : folderCloseImg}
           alt={openFolder ? "Open folder" : "Closed folder"}
-          className="w-6 h-6 ml-2 mr-2 mb-2"/>
+          className="w-6 h-6 ml-2 mr-2" />
         <p className="overflow-x-auto">{folder.name}</p>
       </div>
       {openFolder && (
         <div className="ml-4">
           {folder.decks.map((deck, index) => (
             <div key={index} className="text-black flex items-center">
-              <img src={decksImg} alt="Deck" className="w-10 h-10"/>
-              <div className="overflow-x-auto">
-                <p className="whitespace-nowrap">{deck.name}</p>
+              <div>
+                <Link to={`/decks/${deck.deck_id}`} style={{ display: "flex", alignItems: "center" }}>
+                  <img src={decksImg} alt="Deck" className="w-10 h-10" />
+                  <p className="overflow-x-auto whitespace-nowrap">{deck.name}</p>
+                </Link>
               </div>
             </div>
           ))}
           {folder.children &&
             folder.children.map((child, index) => (
-              <Folder key={index} folder={child} />
+              <Folder key={index} folder={child} className="mt-2" />
             ))}
         </div>
       )}
@@ -45,7 +50,7 @@ const Folder = ({ folder }) => {
 const Sidebar = () => {
   const api = useApi();
   const [sidebarData, setSidebarData] = useState(null);
-  
+
   const fetchSidebarData = () => {
     api._get('/api/sidebar')
       .then((response) => response.json())
@@ -61,19 +66,27 @@ const Sidebar = () => {
     fetchSidebarData();
   }, []);
 
-  console.log(sidebarData);
-
   return (
-    // NOTE: will need to test on other computer who has bigger screen, h-screen looks wrong on my side -- Wilson
-    <div className="w-64 h-[92vh] fixed left-0 bg-gray-200 overflow-x-auto overflow-y-auto">
-      {sidebarData && sidebarData.folders ? (
-        sidebarData.folders.map((folder, index) => (
-          <Folder key={index} folder={folder} />
-        ))
-      ) : (
-        <div>Loading...</div>
-      )}
-    </div>
+    <ResizableBox
+      width={250}
+      height={Infinity}
+      axis="x"
+      resizeHandles={['e']}
+      minConstraints={[100, Infinity]} // Minimum width
+      maxConstraints={[600, Infinity]} // Maximum width
+      className="bg-white h-[92vh]"
+      style={{ overflow: 'hidden', position: 'absolute', left: '0', zIndex: '1' }}
+    >
+      <div className="h-[92vh] overflow-y-auto">
+        {sidebarData && sidebarData.folders ? (
+          sidebarData.folders.map((folder, index) => (
+            <Folder key={index} folder={folder} />
+          ))
+        ) : (
+          <div>Loading...</div>
+        )}
+      </div>
+    </ResizableBox>
   );
 };
 
