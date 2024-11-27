b5f558f0cbca6bba615b3f4a720a98bb0144f365
Tony-Di <77528382+Tony-Di@users.noreply.github.com>
2024-11-27 15:03:57 -0700

    draft of about page

--- frontend/src/pages/AboutPage.jsx
+++ frontend/src/pages/AboutPage.jsx
@@ -1,83 +1,79 @@
-import echoLearnImg from "../assets/EchoLearn.jpg"
 import logoImg from "../assets/echolearn-logo-white.png"
+import CreateCard from "../assets/CreateCard.png"
+import Community from "../assets/Community.png"
+import PublicView from "../assets/PublicDeckView.png"
+import DeckPage from "../assets/DeckPage.png"
+import HomePage from "../assets/TO-DOList.png"
+import Sidebar from "../assets/Sidebar.png"
+import architecture from "../assets/Architecture.png"
 
-const storyText = "EchoLearn is our project for the 2024 Senior Capstone. \
-Memory retention is often a challenge when learning new information. \
-One of our teammates, Seth, enjoys using flashcards to review materials, \
-which inspired an idea: a web-based flashcard system \
-that uses the spaced repetition memorization technique to help users \
-retain knowledge. Our goal is to create a user-friendly \
-and easily accessible tool to make memorization easy.";
+const aboutText = `EchoLearn will provide users with the ability to retain knowledge over an extended period of time.
+This will be accomplished through the use of flashcards and spaced repetition—a memorization technique in which flashcard reviews are spaced at increasing intervals.
+This makes it simple and efficient for users to recall knowledge long after they first learn it.<br>
 
-const teamInfo = [
-  {
-    name: "Di Zhou",
-    roles: ["Backend logic", "Frontend logic"],
-    email: "2279538596@qq.com",
-  },
-  {
-    name: "Seth Boehm",
-    roles: ["API Design", "UI Design", "Deployment and DevOps"],
-    email: "20sboehm@gmail.com",
-  },
-  {
-    name: "Wilson Pan",
-    roles: ["Frontend logic", "UI Design", "Mobile Design"],
-    email: "w1252801556@gmail.com",
-  },
-  {
-    name: "Zengzheng Jiang",
-    roles: ["Database Design", "Testing"],
-    email: "jzzconstantine@gmail.com",
-  }
-]
+The purpose of EchoLearn is to remedy the problem of forgetting the things you’ve learned, whether that be personal information, or knowledge you need for your job or degree.
+It can be thought of as a personal knowledge repository, where you can store everything you want to remember in an intuitive, organized application that is accessible from any device with a web browser.
+
+Some technology we used:
+Front End: React, Tailwind
+Back End: Django Ninja
+Database: SQLite
+Deploy: AWS EC2
+Image storage: AWS S3`;
 
 function AboutPage() {
   return (
     <>
       <div className="flex flex-col items-center w-full h-screen bg-gradient-to-t from-customBlue to-featureBackground">
         <div className="flex flex-row h-24 items-center mt-4 mb-8">
-          <h1 className="text-4xl font-bold text-white mr-4">Team Echolearn</h1>
-          <img src={logoImg} className="w-16 h-16"></img>
+          <h1 className="text-4xl font-bold text-white mr-4">About Page</h1>
         </div>
-        <div className="flex flex-row w-4/5 mx-auto">
-
-          <div className="w-[20%] h-full overflow-y-auto border-r border-black mr-4">
-            <h2 className="text-3xl text-green-300 font-bold mb-10">Team Members</h2>
-            {teamInfo.map((member, index) => (
-              <div key={index} className="text-blue-800">
-                <h3 className="text-xl font-bold mb-2 text-white">{member.name}</h3>
-                <ul className="list-disc ml-5">
-                  {member.roles.map((role, roleIndex) => (
-                    <li key={roleIndex} className="text-sm">{role}</li>
-                  ))}
-                </ul>
-                <p className="mb-6">Email: <a href={`mailto:${member.email}`} className="text-blue-500">{member.email}</a></p>
-              </div>
-            ))}
-          </div>
-
-          <div className="flex flex-col w-[50%] ml-16">
-            <div className="flex flex-row">
-              <img src={echoLearnImg} alt="Echolearn Image" className="w-[75%] h-auto mb-4 rounded-md" />
-              <p className="text-base text-gray-700 m-2">
-                Team members from left to right:
-                <ul className="list-disc text-gray-700 ml-6">
-                  <li>Wilson Pan</li>
-                  <li>Di Zhou</li>
-                  <li>Zengzheng Jiang</li>
-                  <li>Seth Boehm</li>
-                </ul>
-              </p>
-            </div>
-            <p className="text-lg text-black mt-4">
-              {storyText}
+        <div className="flex flex-row w-full px-16">
+          <div className="flex flex-col w-1/2 text-black"> 
+            <p className="pb-4">
+              EchoLearn will provide users with the ability to retain knowledge over an extended period of time.
+              This will be accomplished through the use of flashcards and spaced repetition—a memorization technique in which flashcard reviews are spaced at increasing intervals.
+              This makes it simple and efficient for users to recall knowledge long after they first learn it.
             </p>
+            <p className="pb-4">
+              The purpose of EchoLearn is to remedy the problem of forgetting the things you’ve learned, whether that be personal information, or knowledge you need for your job or degree.
+              It can be thought of as a personal knowledge repository, where you can store everything you want to remember in an intuitive, organized application that is accessible from any device with a web browser. 
+            </p>
+            <p>
+              Some technology we used:
+              <br /> Front End: React, Tailwind
+              <br /> Back End: Django Ninja
+              <br /> Database: SQLite
+              <br /> Deploy: AWS EC2
+              <br /> Image Storage: AWS S3
+            </p>
+            <div className="flex items-center mb-4">
+            <span className="text-white">Architecture</span> 
+              <img src={architecture} className="w-[400px] h-auto max-h-[80vh] "></img>  
+            </div>
+              <div className="flex items-center mb-4">
+                <span className="text-white">Home Page</span> 
+                  <img src={HomePage} className="w-[400px] h-auto max-h-[80vh] "></img>  
+              </div>
+            </div>
+          <div className="flex flex-col w-1/2 items-center">  
+            <div className="flex items-center mb-4">
+            <span className="text-white">Create Card</span>  
+              <img src={CreateCard} className="w-[400px] h-auto max-h-[80vh] "></img>
+            </div>
+            <div className="flex items-center mb-4">
+            <span className="text-white">Community</span>  
+              <img src={Community} className="w-[400px] h-auto max-h-[80vh] "></img>
+            </div>
+            <div className="flex items-center mb-4">
+              <span className="text-white">Deck Page</span>  
+              <img src={DeckPage} className="w-[400px] h-auto max-h-[80vh] "></img>
+            </div>   
           </div>
         </div>
-      </div >
+      </div>
     </>
-  )
+  );
 }
 
 export default AboutPage
